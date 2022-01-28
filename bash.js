37579d3309950d5d49a03ba7a2fef1a946bbb8ca
Joshua Reyes <reyes.j15@gmail.com>
2022-01-27 21:16:49 -0500

    first commit

---
+++ bash.js
@@ -0,0 +1,13 @@
+//output a prompt
+const pwd = require('./pwd');
+const ls = require('./ls');
+
+pwd();
+console.log(ls);
+ls();
+
+// process.stdout.write('prompt > ' );
+// if(cmd === 'pwd') {
+//     process.stdout.write(pwd);
+// }
+
---
+++ pwd.js
@@ -0,0 +1,13 @@
+//output a prompt
+
+
+module.exports = print = () => {
+    process.stdout.write('prompt > ' );
+    process.stdin.on('data', (data) => {
+        const cmd = data.toString().trim();
+        if(cmd === 'pwd') {
+            process.stdout.write(process.cwd());
+            process.stdout.write('prompt > ' );
+        }
+    });
+}
